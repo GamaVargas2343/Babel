ea93810c7a253c822fa4cb738210182d4a8c5458
gamaniel.vargas <gamavargas21@gmail.com>
2025-09-23 20:17:25 +0200

    Se agrego reportes

--- src/test/java/logintest.java
+++ src/test/java/logintest.java
@@ -9,23 +9,30 @@ import org.testng.annotations.*;
 import pages.loginpage;
 import utils.waithelper;
 import utils.screenshothelper;
+
+import java.lang.reflect.Method;
 import java.time.Duration;
 import utils.leerConfig;
+import com.aventstack.extentreports.*;
+import utils.reporteHelper;
+
 
 public class logintest {
     WebDriver driver;
     loginpage page;
+    ExtentTest test;
 
     @BeforeMethod
-    public void inicio(){
+    public void inicio(Method metodo) {
 
         driver = new EdgeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(leerConfig.obtenerTime("timeout")));
         driver.get(leerConfig.obtenerValor("Url"));
         page=new loginpage(driver);
-
+        test=reporteHelper.crearTest(metodo.getName());
 
     }
+
     @DataProvider (name = "Datos" )
     public Object [][] obtenerDatos (){
 
@@ -43,9 +50,15 @@ public class logintest {
     }
     @Test (dataProvider = "Datos")
     public void testLogin(String nombreUsuario,String contrasena , String mensajeEsperado){
-        page.iniciarSesion(nombreUsuario,contrasena);
-        String mensajeObtenido = page.validacionlogin();
-        Assert.assertTrue(mensajeObtenido.contains(mensajeEsperado),"el mensaje esperado no es exitoso");
+        try {
+            page.iniciarSesion(nombreUsuario, contrasena);
+            String mensajeObtenido = page.validacionlogin();
+            Assert.assertTrue(mensajeObtenido.contains(mensajeEsperado), "el mensaje esperado no es exitoso");
+            test.pass("Test paso correctamente");
+        }catch (Exception e) {
+            test.fail("el test ha fallado"+e.getMessage());
+            throw e;
+        }
     }
 
 
@@ -56,5 +69,10 @@ public class logintest {
         driver.quit();
 
     }
+    @AfterSuite
+    public void cerrarReporte(){
+        reporteHelper.generarReporte();
+    }
+
 
 }
